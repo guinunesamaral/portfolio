e03cdfaa9739eb82d538f62055423f78268498dd
guilherme nunes <guinucode@protonmail.com>
2021-03-31 15:41:21 -0300

    Removed the footer components of both Contact and Projects pages

--- src/screens/Contact.js
+++ src/screens/Contact.js
@@ -1,13 +1,12 @@
 import React from "react";
 import { Header } from "../components/Header";
-import { Footer } from "../components/Footer";
+import { ContactForm } from "../components/ContactForm";
 
 export const Contact = () => {
   return (
     <div>
       <Header />
-      Contact
-      <Footer />
+      <ContactForm />
     </div>
   );
 };
--- src/screens/Projects.js
+++ src/screens/Projects.js
@@ -1,13 +1,12 @@
 import React from "react";
 import { Header } from "../components/Header";
-import { Footer } from "../components/Footer";
+import { ProjectList } from "../components/ProjectList";
 
 export const Projects = () => {
   return (
     <div>
       <Header />
-      Projects
-      <Footer />
+      <ProjectList />
     </div>
   );
 };
